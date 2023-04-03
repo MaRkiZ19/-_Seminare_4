969b0db19f2f8685dc5c83bccc1ff329fa05ad6e
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-03 20:11:03 +0300

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,40 @@
+﻿Task1();
+
+void Task1()
+{
+	int number1 = ReadInt("first number");
+	int number2 = ReadInt("second number");
+
+	if (IsMoreThan(number1, number2))
+	{
+		Console.WriteLine($"{number1} > {number2}");
+	}
+	else if (IsMoreThan(number2, number1))
+	{
+		Console.WriteLine($"{number2} > {number1}");
+	}
+	else
+	{
+		Console.WriteLine($"{number1} = {number2}");
+	}
+}
+
+int ReadInt(string argument)
+{
+	int number;
+
+	Console.Write($"Input {argument}: ");
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("It's not a integer number!");
+		Console.Write("Try again: ");
+	}
+
+	return number;
+}
+
+bool IsMoreThan(int number1, int number2)
+{
+	return number1 > number2;
+}
