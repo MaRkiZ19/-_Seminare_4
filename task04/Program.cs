733fcf5fea922311e85097ad65fd1ae215c4a6f5
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-03 21:38:39 +0300

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,23 @@
+﻿//Принимаем число N и выдает произведение чисел от 1 до N. 4->24; 4->120
+
+int Start()
+{
+    System.Console.WriteLine("Введите число");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int a = Start();
+
+int Work(int a)
+{
+    int result = 1;
+    for (int i=1; i<=a; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int result = Work(a);
+System.Console.WriteLine(result);
