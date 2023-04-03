4381f33ff17040d4f7590183d2a769e96bfad59b
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-03 20:46:37 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,20 @@
+﻿int Start()
+{
+    System.Console.WriteLine("Enter number ");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int a = Start();
+int Summ(int a)
+{
+    int result = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int result = Summ(a);
+System.Console.WriteLine(result);
