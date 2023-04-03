82df91e58600d19d1c8bf48320972002a5db4cc6
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-03 20:59:53 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает число и выдает количество цифр в числе
+
+int Read()
+{
+    System.Console.WriteLine("Input number: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int number = Read();
+System.Console.WriteLine();
+string num = number.ToString();
+
+if (number>0)
+{
+    System.Console.WriteLine(num.Length);
+}
+else
+{
+    System.Console.WriteLine(num.Length-1);
+}
